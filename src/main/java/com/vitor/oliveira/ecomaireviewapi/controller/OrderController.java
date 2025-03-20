018c4d2e2decb3a9edcd887e2413711d6e13edfd
Vitor Oliveira <vitinhocastro2@gmail.com>
2025-03-20 00:45:27 -0300

    Testing

--- src/main/java/com/vitor/oliveira/ecomaireviewapi/controller/OrderController.java
+++ src/main/java/com/vitor/oliveira/ecomaireviewapi/controller/OrderController.java
@@ -4,7 +4,6 @@ import com.vitor.oliveira.ecomaireviewapi.DTO.forms.OrderForm;
 import com.vitor.oliveira.ecomaireviewapi.model.Order;
 import com.vitor.oliveira.ecomaireviewapi.service.OrderService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
